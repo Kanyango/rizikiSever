b3d889bb7bdd6459e7a62099933c4358241c2f56
Kanyango <kariukikanyango@gmail.com>
2017-10-05 14:43:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ require('./models')(app, mongoose);
 
 app.set('port' , config.port);
 
-app.use(express.static(__dirname + 'rizikiAdmin/dist'));
+app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
