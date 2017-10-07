4d70adfe95dc53ecfb2151fca47e407e9d7b1c2f
Kanyango <kariukikanyango@gmail.com>
2017-10-07 15:49:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
 //exports.port = process.env.PORT || 8300;
 exports.mongodb = {
 	//uri: 'mongodb://127.0.0.1:27017/rizikiDB'
-	uri: 'mongodb://ndungu:riziki@ds163494.mlab.com:63494/rizikidb';
+	uri: 'mongodb://ndungu:riziki@ds163494.mlab.com:63494/rizikidb'
 };
 exports.secret = 'b7TY?>m6wl_i/<';
