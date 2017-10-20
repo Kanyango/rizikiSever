52cd7056e3ff7bdad9fcf20c50795bd91aea5e78
Kanyango <kariukikanyango@gmail.com>
2017-10-20 16:51:16 +0300

    Update users.js

--- server/schema/users.js
+++ server/schema/users.js
@@ -28,7 +28,7 @@ module.exports = function(app , mongoose)
 	userSchema.methods.setPassword = function(password){
 
 		this.salt = crypto.randomBytes(16).toString('hex');
-		this.hash = crypto.pbkdf2Sync(password , this.salt , 1000 , 64).toString('hex');
+		this.hash = crypto.pbkdf2Sync(password , this.salt , 1000 , 512, 'sha512').toString('hex');
 	};
 	userSchema.methods.validatePassword = function(password) {
      var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 512, 'sha512').toString('hex');
